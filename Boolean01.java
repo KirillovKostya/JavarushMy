687c2e9f93758bf5fa12a80dff9d1a7385c4336e
esarti-life <kirillov-esartiya@ya.ruz>
2018-05-20 21:05:12 +0300

    Add Boolean01.java

---
+++ Boolean01.java
@@ -0,0 +1,10 @@
+package task;
+
+public class Boolean01 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 101; i++) {
+            int number = i % 2;
+            System.out.println(number);
+        }
+    }
+}
